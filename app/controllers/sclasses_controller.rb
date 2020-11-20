3415699f6cfb196b5f9176d1dcec2fa9e700ed6d
xddsyc <xingdd@qlfintech.cn>
2020-11-20 21:41:17 +0800

    完成班级简单增删改查（未改布局）

--- app/controllers/sclasses_controller.rb
+++ app/controllers/sclasses_controller.rb
@@ -1,25 +1,49 @@
 class SclassesController < ApplicationController
 	def index
+		@sclasses = Sclass.all
 	end
 
 	def new
+		@sclass = Sclass.new
 	end
 
 	def create
+		@sclass = Sclass.new(sclass_params)
 
+		if @sclass.save
+			redirect_to @sclass
+		else
+			render 'new'
+		end
 	end
 
 	def edit
+		@sclass = Sclass.find(params[:id])
 	end
 
 	def update
+		@sclass = Sclass.find(params[:id])
+
+		if @sclass.update(sclass_params)
+			redirect_to @sclass
+		else
+			render 'edit'
+		end
 	end
 
 	def show
+		@sclass = Sclass.find(params[:id])
+	end
+
+	def destroy
+		@sclass = Sclass.find(params[:id])
+		@sclass.destroy
+
+		redirect_to sclasses_path
 	end
 
 	private 
 		def sclass_params
-			
+			params.require(:sclass).permit(:grade_name,:class_name)
 		end
 end
