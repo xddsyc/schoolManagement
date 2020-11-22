59f626090332a3d3f5f00e32f94756f7052b7f37
xddsyc <xingdd@qlfintech.cn>
2020-11-22 15:13:28 +0800

    新增用户管理（增删改查）

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+# 后台用户管理
+class UsersController < ApplicationController
+  def index
+	@users = User.order('id desc').page(params[:page]).per(5)
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+
+  	@user.destroy
+
+	redirect_to users_path
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :password, :password_confirmation)
+  	end
+
+end
