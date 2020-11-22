b5b9100616520ab484f83e7ce46b7b17cf7855d8
xddsyc <xingdd@qlfintech.cn>
2020-11-22 12:54:13 +0800

    班级内学号唯一验证

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,5 @@ class Student < ApplicationRecord
 	validates :no, presence: true, length: { minimum: 1}
 	validates :name, presence: true, length: { minimum: 2}
 	validates :sclass_id, presence: true
-	validates :no, uniqueness: true
+	validates :no, uniqueness: { scope: :sclass_id, message: '该班级已存在此学号' }
 end
