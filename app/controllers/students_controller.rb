f347141fe11e9652677123dd8ccfe20c47618f93
xddsyc <xingdd@qlfintech.cn>
2020-11-22 12:34:09 +0800

    新增学生管理增删改查操作

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,25 +1,49 @@
 class StudentsController < ApplicationController
 	def index
+		@students = Student.order('id desc').page(params[:page]).per(5)
 	end
 
 	def new
+		@student = Student.new
 	end
 
 	def create
+		@student = Student.new(student_params)
 
+		if @student.save
+			redirect_to @student
+		else
+			render 'new'
+		end
 	end
 
 	def edit
+		@student = Student.find(params[:id])
 	end
 
 	def update
+		@student = Student.find(params[:id])
+
+		if @student.update(student_params)
+			redirect_to @student
+		else
+			render 'edit'
+		end
 	end
 
 	def show
+		@student = Student.find(params[:id])
+	end
+
+	def destroy
+		@student = Student.find(params[:id])
+		@student.destroy
+
+		redirect_to students_path
 	end
 
 	private 
-		def sclass_params
-			
+		def student_params
+			params.require(:student).permit(:no, :name, :sclass_id)
 		end
 end
