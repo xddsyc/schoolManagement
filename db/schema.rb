bd65c7aee8af9d6d79c425d4afd97d78dcceb1b0
xddsyc <xingdd@qlfintech.cn>
2020-11-22 14:43:05 +0800

    创建user模型，并迁移生成数据库表

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_032400) do
+ActiveRecord::Schema.define(version: 2020_11_22_064005) do
 
   create_table "sclasses", force: :cascade do |t|
     t.string "grade_name"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2020_11_22_032400) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["sclass_id"], name: "index_students_on_sclass_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "students", "sclasses"
