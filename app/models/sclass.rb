17c68db5e629a5de8f603193d79194840fed9a49
xddsyc <xingdd@qlfintech.cn>
2020-11-22 12:49:33 +0800

    添加简单验证

--- app/models/sclass.rb
+++ app/models/sclass.rb
@@ -1,3 +1,5 @@
 class Sclass < ApplicationRecord
-	has_many :students
+	has_many :students, dependent: :destroy
+	validates :grade_name, presence: true, length: { minimum: 2}
+	validates :class_name, presence: true, length: { minimum: 2}
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ApplicationRecord
-  belongs_to :sclass
+  	belongs_to :sclass
+	validates :no, presence: true, length: { minimum: 1}
+	validates :name, presence: true, length: { minimum: 2}
+	validates :sclass_id, presence: true
+	validates :no, uniqueness: true
 end
