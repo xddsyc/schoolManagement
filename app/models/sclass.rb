e3cd84f32cd2e7ce5c03e04a71f74acc5c07be2f
xddsyc <xingdd@qlfintech.cn>
2020-11-23 11:54:30 +0800

    添加班级、用户唯一验证

--- app/models/sclass.rb
+++ app/models/sclass.rb
@@ -2,4 +2,5 @@ class Sclass < ApplicationRecord
 	has_many :students, dependent: :destroy
 	validates :grade_name, presence: true, length: { minimum: 2}
 	validates :class_name, presence: true, length: { minimum: 2}
+	validates :class_name, uniqueness: { scope: :grade_name, message: '此班级已存在' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
 	has_secure_password
+	validates :name, presence: true, length: { minimum: 1}
+	validates :password, presence: true, length: { minimum: 6}
+	validates :name, uniqueness: true
 end
